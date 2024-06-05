90e49ebf747dedc9fca930e2289a8b9a81a024ad
Katalina Caldera <katalinacaldera19@gmail.com>
2024-06-05 14:55:26 -0500

    added cities and a dropdown with the cities on it

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,32 @@ let cities = [
     { name: "Benbrook, TX", latitude: 32.6732, longitude: -97.4606 },
     { name: "Denver, CO", latitude: 39.712179, longitude: -104.973050 },
     { name: "Portland, OR", latitude: 45.520117, longitude: -122.676683 },
-    { name: "Portland, ME", latitude: 43.662093, longitude: -70.260251 }
+    { name: "Portland, ME", latitude: 43.662093, longitude: -70.260251 },
+    { name: "Fort Peck, MT", latitude:48.00855 , longitude: -106.45366},
+    { name: "Ogden, UT", latitude:41.22037 , longitude: -111.97746},
+    { name: "Plain City, UT", latitude:41.29789 , longitude: -112.09397}
+    
  ];
+
 window.onload=()=>{
     // console.log("Wow I am working")
     initCitiesDropdown();
     let citiesDropdown = document.querySelector("#cityoptions")
-    citiesDropdown.addEventListener.addEventListener("change",getlocation);
+    citiesDropdown.addEventListener("change",getLocation);
 
 }
-function getlocation(event){
+function initCitiesDropdown(){
+    
+    let citiesDropdown = document.querySelector("#cityoptions");
+    
+    cities.forEach((city)=>{
+
+        let newCity = document.createElement("option");
+        newCity.value =city.name;
+
+        newCity.textContent = city.name;
+        citiesDropdown.appendChild(newCity);
+    })
+
 
 }
