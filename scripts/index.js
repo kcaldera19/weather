539002e37286718fd1b853de8494721baba140c4
Katalina Caldera <katalinacaldera19@gmail.com>
2024-06-05 14:21:07 -0500

    added a table to the index page

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,17 @@
 "use strict"
+let cities = [
+    { name: "Benbrook, TX", latitude: 32.6732, longitude: -97.4606 },
+    { name: "Denver, CO", latitude: 39.712179, longitude: -104.973050 },
+    { name: "Portland, OR", latitude: 45.520117, longitude: -122.676683 },
+    { name: "Portland, ME", latitude: 43.662093, longitude: -70.260251 }
+ ];
 window.onload=()=>{
-    console.log("Wow I am working")
+    // console.log("Wow I am working")
+    initCitiesDropdown();
+    let citiesDropdown = document.querySelector("#cityoptions")
+    citiesDropdown.addEventListener.addEventListener("change",getlocation);
+
+}
+function getlocation(event){
+
 }
